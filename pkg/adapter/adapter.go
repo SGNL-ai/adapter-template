c1e93f8066473dd902b3005e4c643a09a3fd29c8
shash-sgnl <112989716+shash-sgnl@users.noreply.github.com>
2024-01-09 14:32:52 -0800

    Apply suggestions from code review
    Co-authored-by: Rakshith Raj <16449506+r-rakshith@users.noreply.github.com>

--- pkg/adapter/adapter.go
+++ pkg/adapter/adapter.go
@@ -74,6 +74,7 @@ func (a *Adapter) RequestPageFromDatasource(
 
 		// API Key or OAuth2 Token
 		Token:            request.Auth.HTTPAuthorization,
+		
 		PageSize:         request.PageSize,
 		EntityExternalID: request.Entity.ExternalId,
 		Cursor:           request.Cursor,
